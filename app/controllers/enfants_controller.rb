0b6a01f21131260fd0fc3ec7989484bbd9724520
marcborgna <marc.borgna@gmail.com>
2019-12-25 16:38:52 +0100

    test

--- app/controllers/enfants_controller.rb
+++ app/controllers/enfants_controller.rb
@@ -7,4 +7,20 @@ class EnfantsController < ApplicationController
     @enfant = Enfant.find(params[:id])
     @emails = @enfant.emails.split('/')
   end
+
+
+  def update
+    @enfant = Enfant.find(params[:id])
+    @enfant.update(params_enfant)
+    rep = "lesmaternelles/#{current_user.email}/eleves"
+    Cloudinary::Uploader.upload(params['enfant']['photo'], :use_filename => true, :folder => rep)
+
+  end
+
+  private
+
+  def params_enfant
+    params.require(:enfant).permit(:nom, :prenom, :photo)
+  end
+
 end
